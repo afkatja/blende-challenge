38d794ccc3976fb5cee693f5aa500c99d0904c82
Katja Hollaar <afkatja@gmail.com>
2016-08-30 09:39:38 +0200

    Simplified tests

--- client/main.spec.js
+++ client/main.spec.js
@@ -2,55 +2,21 @@
 // as we are testing with mocha during development
 // visit localhost:3100 to start tests and see the results
 import { chai, expect } from 'meteor/practicalmeteor:chai';
-import { resetDatabase } from 'meteor/xolvio:cleaner';
-import { Factory } from 'meteor/dburles:factory';
 import { Template } from 'meteor/templating';
-import { Blaze } from 'meteor/blaze';
-import { Tracker } from 'meteor/tracker';
-import { faker } from 'faker';
 
 import './main.html';
 import './main.js';
-
 import { Appointments } from '../appointments.js';
 
-// Meteor.methods({
-//   'test.resetDatabase': () => resetDatabase(),
-// });
-
-//create a dummy component for testing
-const withDiv = function withDiv(callback) {
-  const el = document.createElement('div');
-  document.body.appendChild(el);
-  try {
-    callback(el);
-  } finally {
-    document.body.removeChild(el);
-  }
-};
-
-const withRenderedTemplate = function withRenderedTemplate(template, data, callback) {
-  withDiv((el) => {
-    Blaze.renderWithData(template, data, el);
-    Tracker.flush();
-    callback(el);
+describe('appointments app', function () {
+  it('should display today date', function(){
+    let today = new Date();
+    expect(document.body.querySelector('.collection-header h2').innerText).to.equal('30 august 2016');
   });
-};
-
-Factory.define('appointments', Appointments, {});
-
-function callHelper(template, helperName, context, args) {
-  context = context || {};
-  args = args || [];
-  template.__helpers[' ' + helperName].apply(context, args);
-}
-
-describe('appointments app', function (done) {
-  // beforeEach(function (done) {
-  //   Meteor.call('test.resetDatabase', done);
-  // });
   it('should display working hours', function () {
-    callHelper(Template.calendar, 'today');
     expect(document.body.querySelectorAll('.collection-item')).to.have.length(11);
+  });
+  it('should render appointment in the calendar', function(){
+    expect(document.body.querySelectorAll('.appointment')).to.have.length(Appointments.find().count());
   });
 })
